97c70b76851a4748281967271dc2523a4192cfab
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-02-16 18:54:00 +0000

    issue3 star databased modified and created user and session database

--- server/starDatabase.sql
+++ server/starDatabase.sql
@@ -1,19 +1,22 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
 CREATE TABLE stars (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name VARCHAR(255) NOT NULL,
   description TEXT,
-  
+  user_id SERIAL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE users (
-  id        INT PRIMARY KEY,
+  id        SERIAL PRIMARY KEY,
   username  VARCHAR(60) NOT NULL,  
   name      VARCHAR(60) NOT NULL,
   area      VARCHAR(60),
   class     VARCHAR(60),
   role      VARCHAR(20)
 );
+
 
 CREATE TABLE "session" (  "sid" varchar NOT NULL COLLATE "default",  "sess" json NOT NULL,  "expire" timestamp(6) NOT NULL)WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
