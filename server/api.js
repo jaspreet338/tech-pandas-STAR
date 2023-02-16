24819761fbf350c18e123acff0cf48c116c678a2
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-02-16 12:18:57 +0000

    Now fetching GitHub user info

--- server/api.js
+++ server/api.js
@@ -19,14 +19,23 @@ router.get("/auth/github", async( req, res) => {
 	}
 
 	const baseUrl = "https://github.com/login/oauth/access_token";
-	const url = `${baseUrl}?client_id=${config.client_id}&client_secret=${config.client_secret}&code=${code}`;
+	const url = `${baseUrl}?client_id=${config.client_id}&client_secret=${config.client_secret}&code=${code}&scope=read:user`;
     const resp = await fetch (url,
 		{
 			method: "POST",
 			headers: { Accept: "application/json" },
 		});
     const data = await resp.json();
-	res.send(data);
+    logger.debug(JSON.stringify(data));
+	const auth = "Bearer " + data.access_token ;
+	logger.debug(auth);
+	const user_resp = await fetch("https://api.github.com/user", {
+		headers: { Authorization: auth },
+	});
+
+	const gh_user = await user_resp.json();
+
+	res.send(gh_user);
 });
 
 export default router;
