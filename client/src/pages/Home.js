536c55f3f56f5141c17e737aea0a47fa692a724a
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-17 20:18:31 +0000

    added fields in registration component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
 import StarList from "./StarList";
+
 import "./Home.css";
 
 
 export function Home() {
 
 return (
-		<main role="main">
+
 			<div>
 
 				<Navbar />
 				<StarList />
+
 			</div>
-		</main>
 	);
 }
 
--- client/src/pages/RegisterForm.js
+++ client/src/pages/RegisterForm.js
@@ -1,9 +1,54 @@
 import React from "react";
 function RegistrationForm() {
     return (
-        <div>
-
-        </div>
-    );
+			<div>
+				<h2>Register Here</h2>
+				<div>
+					<label className="form_label" htmlFor="name">
+						Name
+					</label>
+					<input
+						className="form_input"
+						type="text"
+						id="name"
+						placeholder="Display Name"
+					></input>
+				</div>
+				<div>
+					<label className="form_label" htmlFor="role">
+						Select Role:
+					</label>
+					<select>
+						<option value="Student">Student</option>
+						<option value="Mentor">Training Mentor</option>
+					</select>
+				</div>
+				<div>
+					<label className="form_label" htmlFor="class">
+						Class
+					</label>
+					<input
+						className="form_input"
+						type="text"
+						id="class"
+						placeholder="Class"
+					></input>
+				</div>
+				<div>
+					<label className="form_label" htmlFor="area">
+						Area
+					</label>
+					<input
+						className="form_input"
+						type="text"
+						id="area"
+						placeholder="Area"
+					></input>
+				</div>
+                <div>
+                    <button type="submit" className="btn">Register</button>
+                </div>
+			</div>
+		);
 }
 export default RegistrationForm;
