412d3fbce3915a5788cc09653acfe06221905f79
FatemRahimi <fa.rahimi5475@gmail.com>
2023-02-18 15:15:00 +0000

    Latest changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,32 +1,31 @@
-import User from "../model/userModel"; // assume you have a user model in a separate file
+import User from "../model/user.model"; // user model
 
 export const register = async (req, res) => {
-  const { Name, Role, Class, Area } = req.body; // assuming the form data is submitted via POST request with these fields
+	const { Name, Role, Class, Area } = req.body; // assuming the form data is submitted via POST request with these fields
 
-  // validate user input
-  if (!Name||Role||Class || !Area) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
+	// validate user input
+	if (!Name || !Role || !Class || !Area) {
+		return res.status(400).json({ message: "All fields are required" });
+	}
 
-  // check if user already exists in the DB
-  const existingUser = await User.findOne({ name: Name });
-  if (existingUser) {
-    return res.status(409).json({ message: "User already exists" });
-  }
+	const user = new User({ Name, Role, Class, Area });
+	try {
+		const newUser = await user.save();
+		return res.status(201).json({
+			message: "User registered successfully",
+			data: newUser,
+		});
+	} catch (error) {
+		return res.status(409).json({ message: error.message });
+	}
+};
 
-  try {
-    // save the user to the DB
-    // create a new user with the provided details
-    const user = await User.create({
-      Name,
-      Role,
-      Class,
-      Area,
-    });
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", data: user });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
-  }
+// read all user in db
+export const getAllUsers = async (req, res) => {
+	try {
+		const users = await User.getAll();
+		return res.status(200).json({ data: users });
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 };
---
+++ server/model/user.model.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import { Pool } from "pg";
+
+const pool = new Pool({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
+	port: process.env.DB_PORT,
+});
+
+class User {
+	constructor({ Name, Role, Class, Area }) {
+		this.Name = Name;
+		this.Role = Role;
+		this.Class = Class;
+		this.Area = Area;
+	}
+
+	async save() {
+		try {
+			// check if user already exists in the DB
+			const { rows: existingUser } = await pool.query(
+				"SELECT * FROM users WHERE Name = $1",
+				[this.Name]
+			);
+			if (existingUser.length) {
+				throw new Error("User already exists");
+			}
+
+			// save the user to the DB
+			const { rows: newUser } = await pool.query(
+				"INSERT INTO users(Name, Role, Class, Area) VALUES ($1, $2, $3, $4) RETURNING *",
+				[this.Name, this.Role, this.Class, this.Area]
+			);
+			return newUser[0];
+		} catch (error) {
+			console.error(error);
+			throw new Error("Internal server error");
+		}
+	}
+
+	static async getAll() {
+		try {
+			const { rows: users } = await pool.query("SELECT * FROM users");
+			return users;
+		} catch (error) {
+			console.error(error);
+			throw new Error("Internal server error");
+		}
+	}
+}
+
+export default User;
