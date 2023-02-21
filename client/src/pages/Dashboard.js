bda399eedb48c143526373426fdaf88f22a5ac0a
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-21 22:36:57 +0000

    import navbar and starList in dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-
+import Navbar from "./Navbar";
+import StarList from "./StarList";
 const  Dashboard = () => {
 	const [name, setName] = useState("");
     useEffect(() => setName("Someone"), []);
 	return (
 		<div>
 			<h1>Hello {name}</h1>
+			<Navbar />
+			<StarList />
 		</div>
 	);
 };
