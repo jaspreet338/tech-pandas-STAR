d714f3c0dee0bceefca943a527daeb5681350631
FatemRahimi <fa.rahimi5475@gmail.com>
2023-02-22 18:33:31 +0000

    verify token

--- server/utils/checkOuth.js
+++ server/utils/checkOuth.js
@@ -3,3 +3,10 @@ import jwt from "jsonwebtoken";
 export const checkAuth = (req, res, next) => {
 	// Get the authentication token from the request headers
 	const token = req.headers.authorization;
+
+    // Verify the token and extract the user ID
+	jwt.verify(token, process.env.SECRET_JWT, (err, decoded) => {
+		if (err) {
+			// If the token is invalid, return a 401 error
+			return res.status(401).json({ error: "Unauthorized" });
+		}
