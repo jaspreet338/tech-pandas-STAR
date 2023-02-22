41a08f5bc06434f0103459f644579007e8075dca
FatemRahimi <fa.rahimi5475@gmail.com>
2023-02-22 18:37:09 +0000

    check user

--- server/utils/checkOuth.js
+++ server/utils/checkOuth.js
@@ -10,3 +10,19 @@ export const checkAuth = (req, res, next) => {
 			// If the token is invalid, return a 401 error
 			return res.status(401).json({ error: "Unauthorized" });
 		}
+	// Check if the user has the appropriate permissions to query STARs
+    if (
+        decoded.role !== "TA" ||
+        decoded.role !== "mentor" ||
+        decoded.role !== "student"
+    ) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Set the user ID in the request object for use in subsequent middleware functions
+    req.userId = decoded.userId;
+
+    // Call the next middleware function
+    next();
+});
+};
