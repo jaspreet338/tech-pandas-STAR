5bb46bfca761fb491802bf9d8d47161dbbfa34ea
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-22 21:51:05 +0000

    Added requested changes

--- client/src/pages/RegisterForm.js
+++ client/src/pages/RegisterForm.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 // import { json } from "react-router-dom";
 function RegisterForm() {
+	const [username, setUsername] = useState("");
 	const [name, setName] = useState("");
 	const [role, setRole] = useState("");
 	const [section, setSection] = useState("");
 	const [area, setArea] = useState("");
 
  function register() {
-		console.warn(name, role, section, area);
+		console.warn(username,name, role, section, area);
 
  }
 	return (
 		<div className="form-container">
 			<h1>Register Here</h1>
 			<div className="form-body">
+				<div>
+					<label className="form_label" htmlFor="name">
+						Username
+					</label>
+					<input
+						className="form_input"
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						id="username"
+						placeholder="type your name"
+					></input>
+				</div>
 				<div>
 					<label className="form_label" htmlFor="name">
 						Name
