2b42aa602777d6d140b0425fa1678890f46dca5e
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-23 19:00:53 +0000

    remove extra bits from nav bar

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -5,9 +5,7 @@ import React from "react";
 		<div className="header">
 			<h3>CYF-STARs</h3>
 			<div className="nav">
-				<a href="/#">Home</a>
-				<a href="/#">Profile</a>
-				<button className="btn-out">Logout</button>
+            <button className="btn-out">Logout</button>
 			</div>
 		</div>
 
