13caf83539c048d5f28fdb01827f0cc308428168
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-23 18:32:43 +0000

    added image for registeration page

--- client/src/pages/RegisterForm.js
+++ client/src/pages/RegisterForm.js
@@ -12,79 +12,82 @@ function RegisterForm() {
 
  }
 	return (
-		<div className="form-container">
-			<h1>Register Here</h1>
-			<div className="form-body">
-				<div>
-					<label className="form_label" htmlFor="name">
-						Username
-					</label>
-					<input
-						className="form_input"
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						id="username"
-						placeholder="type your name"
-					></input>
+		<div>
+
+			<div className="form-container">
+				<h1>Register Here</h1>
+				<div className="form-body">
+					<div>
+						<label className="form_label" htmlFor="name">
+							Username
+						</label>
+						<input
+							className="form_input"
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							id="username"
+							placeholder="type your name"
+						></input>
+					</div>
+					<div>
+						<label className="form_label" htmlFor="name">
+							Name
+						</label>
+						<input
+							className="form_input"
+							type="text"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							id="name"
+							placeholder="Display Name"
+						></input>
+					</div>
+					<div>
+						<label className="form_label" htmlFor="role">
+							Role
+						</label>
+						<select>
+							<option
+								defaultValue={role}
+								onChange={(e) => setRole(e.target.value)}
+							></option>
+							<option value="Student">Student</option>
+							<option value="Mentor">Training Mentor</option>
+						</select>
+					</div>
+					<div>
+						<label className="form_label" htmlFor="section">
+							Class
+						</label>
+						<input
+							className="form_input"
+							type="text"
+							id="class"
+							value={section}
+							onChange={(e) => setSection(e.target.value)}
+							placeholder="Class"
+						></input>
+					</div>
+					<div>
+						<label className="form_label" htmlFor="area">
+							Area
+						</label>
+						<input
+							className="form_input"
+							type="text"
+							id="area"
+							value={area}
+							onChange={(e) => setArea(e.target.value)}
+							placeholder="Area"
+						></input>
+					</div>
 				</div>
 				<div>
-					<label className="form_label" htmlFor="name">
-						Name
-					</label>
-					<input
-						className="form_input"
-						type="text"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						id="name"
-						placeholder="Display Name"
-					></input>
+					<button onClick={register} type="submit" className="btn">
+						Register
+					</button>
 				</div>
-				<div>
-					<label className="form_label" htmlFor="role">
-						Role
-					</label>
-					<select>
-						<option
-							defaultValue={role}
-							onChange={(e) => setRole(e.target.value)}
-						></option>
-						<option value="Student">Student</option>
-						<option value="Mentor">Training Mentor</option>
-					</select>
-				</div>
-				<div>
-					<label className="form_label" htmlFor="section">
-						Class
-					</label>
-					<input
-						className="form_input"
-						type="text"
-						id="class"
-						value={section}
-						onChange={(e) => setSection(e.target.value)}
-						placeholder="Class"
-					></input>
-				</div>
-				<div>
-					<label className="form_label" htmlFor="area">
-						Area
-					</label>
-					<input
-						className="form_input"
-						type="text"
-						id="area"
-						value={area}
-						onChange={(e) => setArea(e.target.value)}
-						placeholder="Area"
-					></input>
-				</div>
-			</div>
-			<div>
-				<button onClick={register} type="submit" className="btn">
-					Register
-				</button>
 			</div>
 		</div>
 	);
