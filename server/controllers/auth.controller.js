22006b6c5abddbdc5640877d3187c89138bc84e5
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-02-24 16:23:19 +0000

    Adjusted registration to update user table

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,19 +1,19 @@
 import User from "../model/user.model"; // user model
 
-export const register = async (req, res) => {
-	const { Name, Role, Class, Area } = req.body; // assuming the form data is submitted via POST request with these fields
+export const updateUser = async (req, res) => {
+	const { username, name, role, section, area } = req.body; // assuming the form data is submitted via POST request with these fields
 
 	// validate user input
-	if (!Name || !Role || !Class || !Area) {
+	if ( !username || !name || !role || !section || !area) {
 		return res.status(400).json({ message: "All fields are required" });
 	}
 
-	const user = new User({ Name, Role, Class, Area });
+	const user = new User( username, name, role, section, area );
 	try {
-		const newUser = await user.save();
+		const updatedUser = await user.update();
 		return res.status(201).json({
 			message: "User registered successfully",
-			data: newUser,
+			data: updatedUser,
 		});
 	} catch (error) {
 		return res.status(409).json({ message: error.message });
