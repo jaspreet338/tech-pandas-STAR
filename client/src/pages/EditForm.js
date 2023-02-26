111d572217ff0192124756efaa5605e3c651fa40
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-26 21:44:24 +0000

    added form

--- client/src/pages/EditForm.js
+++ client/src/pages/EditForm.js
@@ -2,13 +2,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
-function EditForm () {
-    return (
-			<div>
-				<Modal.Header closeButton>
-					<Modal.Title>Your Star </Modal.Title>
-				</Modal.Header>
-			</div>
-		);
+function EditForm() {
+	return (
+		<div>
+			<Modal.Header closeButton>
+				<Modal.Title>Your Star </Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form>
+					<Form.Group controlId="name">
+						<Form.Label>Name:</Form.Label>
+						<Form.Control as="textarea" value="name" />
+					</Form.Group>
+					<Form.Group controlId="situation">
+						<Form.Label>Situation:</Form.Label>
+						<Form.Control as="textarea" value="situation" />
+					</Form.Group>
+					<Form.Group controlId="task">
+						<Form.Label>Task:</Form.Label>
+						<Form.Control as="textarea" value="task" />
+					</Form.Group>
+					<Form.Group controlId="action">
+						<Form.Label>Action:</Form.Label>
+						<Form.Control as="textarea" value="action" />
+					</Form.Group>
+					<Form.Group controlId="result">
+						<Form.Label>Result:</Form.Label>
+						<Form.Control as="textarea" value="result" />
+					</Form.Group>
+					<Form.Group controlId="content">
+						<Form.Label>Description :</Form.Label>
+						<Form.Control as="textarea" value="description" />
+					</Form.Group>
+				</Form>
+			</Modal.Body>
+		</div>
+	);
 }
 export default EditForm;
