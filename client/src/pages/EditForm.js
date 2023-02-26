450e3211f3d61080aca97223c761108fc37e4188
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-26 21:14:18 +0000

    added new component to edit form

---
+++ client/src/pages/EditForm.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+function EditForm () {
+    return (
+			<div>
+				<Modal.Header closeButton>
+					<Modal.Title>Your Star </Modal.Title>
+				</Modal.Header>
+			</div>
+		);
+}
+export default EditForm;
