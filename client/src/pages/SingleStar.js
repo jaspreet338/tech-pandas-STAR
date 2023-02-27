4e32079bc4c650db4d616783731639dcc6277863
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-02-27 20:43:20 +0000

    added cards to single star using react bootstrap

--- client/src/pages/SingleStar.js
+++ client/src/pages/SingleStar.js
@@ -1,30 +1,41 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
-const SingleStar = ({ star , id }) => {
+const SingleStar = ({ star }) => {
 	return (
-		<div key={id}>
-			<h3>{star.name}</h3>
-			<p>
-				<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Situation :</span>
-				{star.situation}
-			</p>
-      <p>
-				<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Task:</span>
-				{star.task}
-			</p>
-      <p>
-				<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Action:</span>
-				{star.action}
-			</p>
-      <p>
-				<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Result:</span>{" "}
-				{star.result}
-			</p>
-      <p>
-				<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Description:</span>{" "}
-				{star.description}
-			</p>
-		</div>
+		<Card
+			className="my-3 p-3 rounded"
+			border="dark"
+			style={{ boxShadow: "0 2px 5px rgba(0,0,0,0.3)" }}
+		>
+			<Card.Body>
+				<Card.Title>{star.name}</Card.Title>
+				<Card.Text>
+					<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+						Situation :
+					</span>
+					{star.situation}
+				</Card.Text>
+				<Card.Text>
+					<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Task:</span>
+					{star.task}
+				</Card.Text>
+				<Card.Text>
+					<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Action:</span>
+					{star.action}
+				</Card.Text>
+				<Card.Text>
+					<span style={{ fontSize: "1rem", fontWeight: "bold" }}>Result:</span>{" "}
+					{star.result}
+				</Card.Text>
+				<Card.Text>
+					<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+						Description:
+					</span>{" "}
+					{star.description}
+				</Card.Text>
+			</Card.Body>
+		</Card>
 	);
 };
 
