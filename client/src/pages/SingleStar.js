70b1d85ebdfeee386aa1ffe211d2a7ff7f343413
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-03-06 12:18:31 +0000

    Added Conditional statements for view

--- client/src/pages/SingleStar.js
+++ client/src/pages/SingleStar.js
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-const SingleStar = ({ star, setStars }) => {
+const SingleStar = ({ user, star, setStars }) => {
   const [editing, setEditing] = useState(false);
   const refreshStars = (refresh) => {
     if (refresh) {
@@ -13,47 +13,68 @@ const SingleStar = ({ star, setStars }) => {
     }
     setEditing(null);
   };
-  return (
-    <Card key={star.id} className="my-3 shadow">
-      <Card.Body>
-        <Row>
-          <Col sm={12} md={6}>
-            <Card.Title>{star.name}</Card.Title>
-            <Card.Text>
-              <span style={{ fontSize: "1rem", fontWeight: "bold" }}>
-                Situation:
-              </span>{" "}
-              {star.situation}
-            </Card.Text>
-            <Card.Text>
-              <span style={{ fontSize: "1rem", fontWeight: "bold" }}>Task:</span>{" "}
-              {star.task}
-            </Card.Text>
-            <Card.Text>
-              <span style={{ fontSize: "1rem", fontWeight: "bold" }}>Action:</span>{" "}
-              {star.action}
-            </Card.Text>
-            <Card.Text>
-              <span style={{ fontSize: "1rem", fontWeight: "bold" }}>Result:</span>{" "}
-              {star.result}
-            </Card.Text>
-            <Card.Text>
-              <span style={{ fontSize: "1rem", fontWeight: "bold" }}>
-                Description:
-              </span>{" "}
-              {star.description}
-            </Card.Text>
-          </Col>
-          <Col sm={12} md={6} className="d-flex justify-content-center align-items-center">
-            <Button onClick={() => setEditing(true)} className="ml-">
-              Edit STAR
-            </Button>
-            <EditForm active={editing} star={star} refreshStars={refreshStars} />
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
-  );
-};
 
-export default SingleStar;
+
+
+  return (
+		<Card key={star.id} className="my-3 shadow">
+			<Card.Body>
+				<Row>
+					<Col sm={12} md={6}>
+						<Card.Title>{star.name}</Card.Title>
+						<Card.Text>
+							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+								Situation:
+							</span>{" "}
+							{star.situation}
+						</Card.Text>
+						<Card.Text>
+							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+								Task:
+							</span>{" "}
+							{star.task}
+						</Card.Text>
+						<Card.Text>
+							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+								Action:
+							</span>{" "}
+							{star.action}
+						</Card.Text>
+						<Card.Text>
+							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+								Result:
+							</span>{" "}
+							{star.result}
+						</Card.Text>
+						<Card.Text>
+							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+								Description:
+							</span>{" "}
+							{star.description}
+						</Card.Text>
+					</Col>
+					<Col
+						sm={12}
+						md={6}
+						className="d-flex justify-content-center align-items-center"
+					>
+						{user.role === "TA" || user.role === "mentor" ? null : (
+							<>
+								<Button onClick={() => setEditing(true)} className="ml-">
+									Edit STAR
+								</Button>
+								<EditForm
+									active={editing}
+									star={star}
+									refreshStars={refreshStars}
+								/>
+							</>
+						)}
+					</Col>
+				</Row>
+			</Card.Body>
+		</Card>
+	);
+ };
+
+ export default SingleStar;
