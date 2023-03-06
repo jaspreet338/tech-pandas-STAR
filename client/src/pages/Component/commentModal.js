783599940eca65813e87d9e770c2fcd80ce70067
FatemRahimi <fa.rahimi5475@gmail.com>
2023-03-06 00:27:07 +0000

    Call the onSubmit function to add the comment

--- client/src/pages/Component/commentModal.js
+++ client/src/pages/Component/commentModal.js
@@ -1,19 +1,47 @@
 import React, { useState } from "react";
-import EditForm from "./EditForm";
+import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Badge from "react-bootstrap/Badge";
-import ListGroup from "react-bootstrap/ListGroup";
-import CommentModal from "./Component/CommentModal";
+import Form from "react-bootstrap/Form";
 
-const SingleStar = ({ star, setStars }) => {
-	const [editing, setEditing] = useState(false);
-	const [showCommentModal, setShowCommentModal] = useState(false);
-	const refreshStars = (refresh) => {
-		if (refresh) {
-			setStars(null);
-		}
-		setEditing(null);
+const CommentModal = ({ show, onHide, onSubmit }) => {
+	const [commentText, setCommentText] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// Create a new comment object with the current date and time
+		const newComment = {
+			text: commentText,
+			commenter: "Anonymous",
+			datetime: new Date().toLocaleString(),
+			id: Math.floor(Math.random() * 10000), // generate a random id for the comment
+		};
+		// Call the onSubmit function to add the comment
+		onSubmit(newComment);
+		// Clear the comment text
+		setCommentText("");
 	};
+
+	return (
+		<Modal show={show} onHide={onHide}>
+			<Modal.Header closeButton>
+				<Modal.Title>Comments</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group controlId="commentText">
+						<Form.Control
+							as="textarea"
+							placeholder="Add a comment..."
+							value={commentText}
+							onChange={(e) => setCommentText(e.target.value)}
+						/>
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Add Comment
+					</Button>
+				</Form>
+			</Modal.Body>
+		</Modal>
+	);
+};
+export default CommentModal;
