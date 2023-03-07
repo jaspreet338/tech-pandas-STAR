9baf7eb389e9c5021db699ae4c975307815a7a5d
FatemRahimi <fa.rahimi5475@gmail.com>
2023-03-07 21:04:01 +0000

    deleted stardetail

--- client/src/pages/Component/StarDetail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-const StarDetail=()=>{
-    const location = useLocation().state;
-    return <div>{location.id}</div>;
-};
-export default StarDetail;
