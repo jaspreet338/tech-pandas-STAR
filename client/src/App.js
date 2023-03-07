bae2807dbf7a0033d83494d0a78bbc385c70c370
FatemRahimi <fa.rahimi5475@gmail.com>
2023-03-07 22:43:13 +0000

    edit endre new file in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import RegisterForm from "./pages/RegisterForm";
-import StarDetail from "./pages/Component/StarDetail";
+import SingleStarPage from "./pages/SingleStarPage";
 
 const App = () => (
 	<Routes>
@@ -11,7 +11,7 @@ const App = () => (
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/dashboard" element={<Dashboard />} />
 		<Route path="/register" element={<RegisterForm />} />
-		<Route path="/star/:id" element={<StarDetail />} />
+		<Route path="/star/:id" element={ <SingleStarPage />} />
 	</Routes>
 );
 export default App;
