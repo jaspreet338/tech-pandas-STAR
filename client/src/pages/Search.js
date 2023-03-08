2752c114e028117521082240f1bc1bab3c29803e
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-03-08 17:03:19 +0000

    search modified

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,36 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
+const Search = ({ updateFilter ,placeholder }) => {
+	const [input, setInput] = useState("");
 
-const Search = ({ setSearch, placeholder }) => {
-	const [searchString, setSearchString] = useState("");
-
-	// function to handle search from submission
-	const handleSearchSubmit = (e) => {
-		e.preventDefault();
-		setSearch(searchString);
+	const handleSearch = (e) => {
+		setInput(e.target.value);
+		updateFilter(e.target.value);
 	};
-
-	// function to handle show all button click
-	const handleShowAllClick = () => {
-		setSearchString("");
-		setSearch("");
-	};
-
-
+  const clearSearch = () =>{
+	setInput("");
+	updateFilter("");
+  };
 	return (
-		<div>
-			<form onSubmit={handleSearchSubmit}>
-				<input
-					type="text"
-					value={searchString}
-					onChange={(event) => setSearchString(event.target.value)}
-					placeholder={placeholder}
-				/>
-				<button type="submit">Search</button>
-			</form>
-			{searchString !== "" && (
-				<button onClick={handleShowAllClick}>Show all</button>
-			)}
+		<div className="input-group mt-10">
+			<input
+				type="search"
+				className="form-control rounded"
+				placeholder={placeholder}
+				aria-label={placeholder}
+				aria-describedby="search-addon"
+				onChange={handleSearch}
+				value={input}
+			/>
+			<button type="button" className="btn btn-outline-primary" onClick={clearSearch}>
+				x
+			</button>
 		</div>
 	);
 };
--- client/src/pages/StarList.js
+++ client/src/pages/StarList.js
@@ -7,7 +7,6 @@ const StarList = () => {
 	const [stars, setStars] = useState(null);
 	const [error, setError] = useState(false);
 	const [loading, setLoading] = useState(true);
-	const [userSearch, setUserSearch] = useState("");
 	const [starSearch, setStarSearch] = useState("");
 
 	useEffect(() => {
@@ -33,34 +32,33 @@ const StarList = () => {
 		}
 	}, [stars]);
 	let filteredStars = stars ? stars : [];
-	if (userSearch.trim() !== "") {
-		filteredStars = filteredStars.filter((star) =>
-			star.name.includes(userSearch.trim())
-		);
-	}
+
 	if (starSearch.trim() !== "") {
+		console.log(starSearch);
+		const searchLowerCase = starSearch.trim().toLowerCase(); // convert search input to lowercase
 		filteredStars = filteredStars.filter(
 			(star) =>
-				star.situation.includes(starSearch.trim()) ||
-				star.task.includes(starSearch.trim()) ||
-				star.action.includes(starSearch.trim()) ||
-				star.result.includes(starSearch.trim())
+			star.name.toLowerCase().includes(searchLowerCase) ||
+				star.situation.toLowerCase().includes(searchLowerCase) || // convert star properties to lowercase before comparison
+				star.task.toLowerCase().includes(searchLowerCase) ||
+				star.action.toLowerCase().includes(searchLowerCase) ||
+				star.result.toLowerCase().includes(searchLowerCase)
 		);
 	}
+
 	return (
 		<div className="star-container">
 			<h2>Welcome to your Star List</h2>
 			<AddForm setStars={setStars} />
-			<Search setSearch={setUserSearch} placeholder="filter users" />
-			<Search setSearch={setStarSearch} placeholder="filter stars" />
+			<Search updateFilter={setStarSearch} placeholder="search stars" />
 			<ul>
 				{loading && <span>Loading, please wait until stars loads...</span>}
 				{error && <span>{"There is a problem fetching the  data "}</span>}
 				{filteredStars.map((star) => (
-						<li id="list-item" key={star.id}>
-							<SingleStar star={star} setStars={setStars} />
-						</li>
-					))}
+					<li id="list-item" key={star.id}>
+						<SingleStar star={star} setStars={setStars} />
+					</li>
+				))}
 			</ul>
 		</div>
 	);
