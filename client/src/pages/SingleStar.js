abf9a2e1d35a604e11b0a1e47ac5324535277724
jaspreet kaur <kaurjas1928@yahoo.com>
2023-03-14 23:25:36 +0000

    correct variable name

--- client/src/pages/SingleStar.js
+++ client/src/pages/SingleStar.js
@@ -22,10 +22,10 @@ const SingleStar = ({ user, star, setStars }) => {
 			<Card.Body>
 				<Row>
 					<Col sm={12} md={6}>
-                         <Link to={`/star/${star.id}`}>
+						<Link to={`/star/${star.id}`}>
 							<Card.Title>{star.name}</Card.Title>
-							<Card.Text>{star.creator_name}</Card.Text>
 						</Link>
+						<Card.Text>{star.creator}</Card.Text>
 						<FavouriteIcon star={star} />
 						<Card.Text>
 							<span style={{ fontSize: "1rem", fontWeight: "bold" }}>
