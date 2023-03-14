c682c758db7f7b305a9882d94426b563574503cc
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-03-14 00:05:36 +0000

    removed jpg

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -18,7 +18,7 @@ module.exports = {
 				},
 			},
 			{
-				test: /\.(png|jpg|svg|jpe?g|gif)$/,
+				test: /\.(png|svg|jpe?g|gif)$/,
 				// loader: "file-loader",
 				type: "asset/resource",
 			},
