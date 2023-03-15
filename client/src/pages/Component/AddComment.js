a7ea0ee9c5dabf0a47a18a3dfd0e543aaf04696b
FatemRahimi <fa.rahimi5475@gmail.com>
2023-03-15 16:05:34 +0000

    remove of different branch

--- client/src/pages/Component/AddComment.js
+++ client/src/pages/Component/AddComment.js
@@ -3,7 +3,6 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
-import "./AddComment.css";
 
 const AddComment = ({ star, setStar }) => {
     const [comment, setComment] = useState("");
