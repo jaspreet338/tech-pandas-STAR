1db5d0f8df4c82bc3c54b91148fa0eadba8e9cfd
FatemRahimi <fa.rahimi5475@gmail.com>
2023-03-16 13:47:45 +0000

    modify css

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./NavbarSingleStar.css";
 
 
 const CustomNavbar = ({ children }) => (
