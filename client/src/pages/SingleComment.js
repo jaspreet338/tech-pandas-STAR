f3e549f2dd30fe7f5ab42b6555397afaa6620d11
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-03-17 14:32:06 +0000

    Added moment

--- client/src/pages/SingleComment.js
+++ client/src/pages/SingleComment.js
@@ -2,6 +2,7 @@ import { Card } from "react-bootstrap";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import EditComment from "./Component/EditComment";
+import moment from "moment";
 import "./SingleStarView.css";
 
 function SingleComment({ comment, refreshStar, user }) {
@@ -18,22 +19,21 @@ function SingleComment({ comment, refreshStar, user }) {
 		<div className="my-3">
 			<Card body className="comment-body">
 				<Card.Subtitle className="mb-2 text-muted">
-					{comment.commenter} {comment.created_at}
+					{comment.commenter} on {moment(comment.created_at).format("Do MMMM h:mm a")}
 				</Card.Subtitle>
 				<Card.Text>{comment.comment}</Card.Text>
-				{user.id === comment.user_id? (
-				<>
-				<div className="mb-3 Edit">
-					<Button onClick={() => setEditing(true)}>Edit</Button>
-				</div>
-				<EditComment
-					comment={comment}
-					active={editing}
-					refreshComment={refreshComment}
-				/>
-				</>
-				) : null
-				}
+				{user.id === comment.user_id ? (
+					<>
+						<div className="mb-3 Edit">
+							<Button onClick={() => setEditing(true)}>Edit</Button>
+						</div>
+						<EditComment
+							comment={comment}
+							active={editing}
+							refreshComment={refreshComment}
+						/>
+					</>
+				) : null}
 			</Card>
 		</div>
 	);
